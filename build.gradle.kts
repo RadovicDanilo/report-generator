9dace1fdd677aa0279f4214f90fa8cf03957c24a
Danilo <daniloradovic2003@gmail.com>
2024-10-28 13:32:31 +0100

    Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,24 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
+}
+
+subprojects {
+    apply(plugin = "kotlin")
+
+    dependencies {
+        implementation(kotlin("stdlib"))
+        // Add other common dependencies here
+    }
+
+    // Configure source sets for each subproject
+    sourceSets {
+        val main by getting {
+            kotlin.srcDir("src/main/kotlin")
+            resources.srcDir("src/main/resources")
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,10 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "softverske-komponente-1"
-include("exporter")
-include("exporter-txt")
-include("exporter-pdf")
-include("exporter-csv")
-include("exporter-xls")
-include("app")
+
+include(":app")
+include(":exporter")
+include(":exporter-txt")
+include(":exporter-pdf")
+include(":exporter-csv")
+include(":exporter-xls")
