a1873ddf5e6648655a0a46ff6672cee8dee19400
Danilo <daniloradovic2003@gmail.com>
2024-10-29 12:15:49 +0100

    fixes

--- exporter/src/main/kotlin/ReportExporter.kt
+++ exporter/src/main/kotlin/ReportExporter.kt
@@ -2,8 +2,7 @@ package src.main.kotlin
 
 import src.main.kotlin.file.File
 
-abstract class ReportExporter(
-    val exporterType: String,
-) {
+abstract class ReportExporter {
+    abstract val exporterType: String
     abstract fun export(file: File)
 }
