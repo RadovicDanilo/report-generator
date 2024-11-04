1196c6a133dc6379003f1a18b964d32507768e13
Danilo <daniloradovic2003@gmail.com>
2024-11-04 23:59:59 +0100

    app start

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -1,13 +1,28 @@
 import src.main.kotlin.ReportExporter
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
 import java.util.ServiceLoader
 
 fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        throw IllegalArgumentException("Connection string required")
+    }
+
+    val connectionString = args[0]
+
+    try {
+        val connection: Connection = DriverManager.getConnection(connectionString)
+        println("Connected to the database successfully!")
+    } catch (e: SQLException) {
+        e.printStackTrace()
+        println("Failed to connect to the database.")
+    }
+
     val serviceLoader = ServiceLoader.load(ReportExporter::class.java)
 
     println("Registered services\n")
-    serviceLoader.forEach { pushService ->
-        println("   exporter type: ${pushService.exporterType}")
-        println("   exporter file extensions: ${pushService.fileExtension}\n")
-    }
+    val exporters = serviceLoader.map { exporter -> exporter }
+
 
 }
